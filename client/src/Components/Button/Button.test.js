fb3ae1bf7c9cb32e77915e7d39c42a092b118846
lgutie16 <lindaandgod@gmail.com>
2018-01-28 21:48:09 -0500

    WIP plop file

---
+++ client/src/Components/Button/Button.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import expect from 'expect'
+import { mount, shallow } from 'enzyme'
+import TestUtils from 'react-addons-test-utils'
+import Button from './Button'
+
+describe(' Button should render: ', ()=>{
+    it(' Button is rendering', () =>{
+        //Test stuff
+        //expect(/**/).toBe(/**/)
+    })
+})
